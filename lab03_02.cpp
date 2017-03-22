f1bbacdd80b23effc5900f5e39b629e41faff979
Sreehari B S <sreeharibs95@gmail.com>
2017-03-22 20:56:12 +0530

    3.2 Animate Ship - using translate

---
+++ lab03_02.cpp
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<GL/glut.h>
+
+int i=0;
+
+void init(){
+
+    glClearColor( 0.0,0.0,0.0,1.0 );
+    gluOrtho2D( 0 , 100 , 0 , 100 );
+
+}
+
+void display(){
+
+    glClear( GL_COLOR_BUFFER_BIT );
+
+        glTranslatef( 1.0 , 0.0 , 0.0 );
+
+    //static - ship
+    glBegin(GL_POLYGON);
+
+            glColor3f( 1.0 , 0.0 , 0.0 );
+
+            glVertex2i( 10 , 30 );
+            glVertex2i( 40 , 30 );
+            glVertex2i( 35 , 20 );
+            glVertex2i( 15 , 20 );
+
+    glEnd();
+
+    glutSwapBuffers();
+
+}
+
+void reshape( int w , int h ){
+
+    glViewport( 0 , 0 , w , h  );
+
+}
+
+int main( int argc, char **argv ){
+
+    glutInit( &argc , argv );
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA );
+    glutInitWindowSize( 650 , 650 );
+    glutInitWindowPosition( 10 , 10 );
+    glutCreateWindow( "My Ship" );
+
+    init();
+
+    //callback functions
+    glutDisplayFunc( display );
+    glutIdleFunc( display );
+    glutReshapeFunc(reshape);
+
+    glutMainLoop();
+
+}
